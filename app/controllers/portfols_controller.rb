ea4600ad32c7f153cc997a5fabf665d0b1ed30e6
vijayda1891 <vijayda2009@gmail.com>
2019-09-20 13:09:42 +0530

    Integrated new and create action for portfolio

--- app/controllers/portfols_controller.rb
+++ app/controllers/portfols_controller.rb
@@ -3,4 +3,19 @@ class PortfolsController < ApplicationController
 	def index
 		@portfolio_items = Portfol.all
 	end
+
+	def new
+		@portfol = Portfol.new
+	end
+
+	def create
+		@portfol = Portfol.new(params.require(:portfol).permit(:title, :subtitle, :body))
+		respond_to do |format|
+			if @portfol.save
+				format.html { redirect_to portfols_path, notice: "Portfolio created." }
+			else
+				format.html { redirect_to :new }
+			end
+		end
+	end
 end
