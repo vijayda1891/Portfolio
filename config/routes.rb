4230511c25d6862fdae0416698920096aab85b5b
vijayda1891 <vijayda2009@gmail.com>
2019-09-20 17:44:41 +0530

    Customized Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :portfols
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  resources :portfols, except: [:show]
+  get 'portfol/:id', to: 'portfols#show', as: :portfol_show
+  
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'  
 end
