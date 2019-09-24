851a78f5dd6f16f1fa6572d5b47b27f591bfbcaf
vijayda1891 <vijayda2009@gmail.com>
2019-09-24 11:53:15 +0530

    Added custom scopes for portfols

--- app/models/portfol.rb
+++ app/models/portfol.rb
@@ -1,3 +1,9 @@
 class Portfol < ApplicationRecord
 	validates_presence_of :title, :body, :main_image, :thumb_image
+
+	def self.bayern
+		where(subtitle: 'Bayern')
+	end
+
+	scope :robben, -> { where(subtitle: 'Robben') } 
 end
