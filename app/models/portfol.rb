3a9079cfda695a73e430adf9840caee543947e6a
vijayda1891 <vijayda2009@gmail.com>
2019-10-09 17:08:33 +0530

    Implemented concerns for image data management

--- app/models/portfol.rb
+++ app/models/portfol.rb
@@ -1,4 +1,5 @@
 class Portfol < ApplicationRecord
+	include Placeholder
 	validates_presence_of :title, :body, :main_image, :thumb_image
 
 	def self.bayern
@@ -10,7 +11,7 @@ class Portfol < ApplicationRecord
 	after_initialize :set_defaults
 
 	def set_defaults
-		self.main_image ||= "https://via.placeholder.com/600x400"
-		self.thumb_image ||= "https://via.placeholder.com/350x200"
+		self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+		self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
 	end
 end
