e35d36da748912ab46c9fc0b9d7532488c25e7f0
vijayda1891 <vijayda2009@gmail.com>
2019-10-10 18:04:36 +0530

    Customized login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_name: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfols, except: [:show]
   get 'portfol/:id', to: 'portfols#show', as: :portfol_show
   
