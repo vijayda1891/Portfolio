e6490f775337f4b4fee75f474fc1b1ef9cf70d47
vijayda1891 <vijayda2009@gmail.com>
2019-11-12 12:42:35 +0530

    Implemented dynamic nav bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,55 @@ module ApplicationHelper
     def copyright_generator
         DavijayViewTool::Renderer.copyright 'DA Vijay', 'Rights Reserved'
     end
+
+    def nav_items
+        [
+            {
+                url: root_path,
+                title: 'Home'
+
+            },
+             {
+                url: about_path,
+                title: 'About'
+
+            },
+             {
+                url: contact_path,
+                title: 'Contact'
+
+            },
+             {
+                url: blogs_path,
+                title: 'Blog'
+
+            },
+             {
+                url: portfols_path,
+                title: 'Portfolio'
+
+            }
+        ]
+
+    end
+
+    def nav_helper style, tag_type
+# nav_links = <<NAV
+# <#{tag_type}><a href="#{root_path}" class="#{style} #{active? root_path}">Home</a></#{tag_type}>
+# <#{tag_type}><a href="#{about_path}" class="#{style} #{active? about_path}">About</a></#{tag_type}>
+# <#{tag_type}><a href="#{contact_path}" class="#{style} #{active? contact_path}">Contact</a></#{tag_type}>
+# <#{tag_type}><a href="#{blogs_path}" class="#{style} #{active? blogs_path}">Blog</a></#{tag_type}>
+# <#{tag_type}><a href="#{portfols_path}" class="#{style} #{active? portfols_path}">Portfolio</a></#{tag_type}>
+# NAV --------> heredoc way for writing multiline code
+
+    nav_links = ''
+    nav_items.each do |item|
+        nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end 
+    nav_links.html_safe
+    end
+
+    def active? path
+        "active" if current_page? path
+    end
 end
