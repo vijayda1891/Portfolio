38131a6bc26f301b0fdc52413947e9f7ffeed4c5
vijayda1891 <vijayda2009@gmail.com>
2019-11-26 13:52:21 +0530

    made changes to application.rb

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module Portfolio
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.eager_load_paths << "#{Rails.root}/lib"
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
