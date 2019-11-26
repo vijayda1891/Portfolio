2def8379edd7bf0a1b7f2bce7f6a8aa69985d6b1
vijayda1891 <vijayda2009@gmail.com>
2019-11-26 17:04:33 +0530

    fixed js issue and added code for action cable to work in production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require rails-ujs
 //= require jquery3
 //= require popper
-//= require bootstrap-sprockets
 //= require activestorage
 //= require jquery-ui
 //= require html.sortable
@@ -22,3 +21,4 @@
 //= require cable
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
